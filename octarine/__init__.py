c976642daa89a9ef7f4c7c323eff25662bb08908
Philipp Schlegel <observing@web.de>
2024-04-17 16:18:23 +0100

    register plugins inside plugins.py

--- octarine/__init__.py
+++ octarine/__init__.py
@@ -1,7 +1,4 @@
 from . import utils
 from .viewer import *
 from .conversion import register_converter, get_converter
-from .plugins import register_plugins
-
-
-register_plugins()
+from . import plugins
