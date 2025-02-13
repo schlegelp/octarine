2a811ade4099acbfe722af13c80f150b724d10d7
Philipp Schlegel <observing@web.de>
2025-02-13 10:08:16 +0000

    Setup.py: add flags for 3.12 and 3.13

--- setup.py
+++ setup.py
@@ -47,6 +47,8 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     install_requires=install_requires,
     extras_require=dict(extras_require),
