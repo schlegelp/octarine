4a1468b5b9d9c04285f4e8dfe9c7016cf2e9370e
Philipp Schlegel <observing@web.de>
2026-04-07 13:22:29 +0100

    make controls module available at top level

--- octarine/__init__.py
+++ octarine/__init__.py
@@ -2,4 +2,4 @@ from . import utils
 from .viewer import *
 from .__version__ import __version__
 from .conversion import register_converter, get_converter
-from . import plugins
+from . import plugins, controls
